e0f24484315b7ba65db25148b55c4614404612b5
2pacbiggie25 <cristopherlanza@gmail.com>
2021-11-19 09:48:07 -0600

    avance menu, orden y plato
    Cristopher y Jhonathan

--- Proyecto 3parcial/app/src/main/java/com/example/proyecto3parcial/menu.java
+++ Proyecto 3parcial/app/src/main/java/com/example/proyecto3parcial/menu.java
@@ -2,7 +2,9 @@ package com.example.proyecto3parcial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class menu extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void irPlatillo(View view) {
+
+        Intent botonPlatillo = new Intent(this, platillo.class);
+
+        startActivity(botonPlatillo);
+
+    }
+
+    public void verOrden(View view) {
+
+        Intent botonOrden = new Intent(this, ordenes.class);
+
+        startActivity(botonOrden);
     }
 }
