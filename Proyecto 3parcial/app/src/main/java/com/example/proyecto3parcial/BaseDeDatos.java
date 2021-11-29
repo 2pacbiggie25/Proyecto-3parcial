d8434a6b0c4ff57e401cba5159ea4778ebe5a52a
2pacbiggie25 <cristopherlanza@gmail.com>
2021-11-29 17:51:38 -0600

    Creacion base de datos
    Base de Datos Creacion 29/11/2021

---
+++ Proyecto 3parcial/app/src/main/java/com/example/proyecto3parcial/BaseDeDatos.java
@@ -0,0 +1,26 @@
+package com.example.proyecto3parcial;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BaseDeDatos extends SQLiteOpenHelper
+{
+
+    public BaseDeDatos(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase Restaurantedb) {
+        Restaurantedb.execSQL("create table TipoUsuario (codigo int primary key autoincrement, tipoUsuario boolean)");
+        Restaurantedb.execSQL("create table Usuario (codigo int primary key autoincrement,idTipoUsuario int, nombre text, apellido text, correo text, usuario text, password text, foreign key(idTipoUsuario)references tipoUsuario(codigo) )");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase Restaurantedb, int oldVersion, int newVersion) {
+
+    }
+}
