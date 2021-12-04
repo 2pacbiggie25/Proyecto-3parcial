83867e7707fb95ec1cef83ed2ceb2d9be3abf08b
JessyLDA <hinadiaz15@gmail.com>
2021-12-04 11:20:03 -0800

    listview ordenes
    no se s

--- Proyecto 3parcial/app/src/main/java/com/example/proyecto3parcial/BaseDeDatos.java
+++ Proyecto 3parcial/app/src/main/java/com/example/proyecto3parcial/BaseDeDatos.java
@@ -2,10 +2,13 @@ package com.example.proyecto3parcial;
 
 /*import android.content.ContentValues;*/
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class BaseDeDatos extends SQLiteOpenHelper
 {
@@ -19,12 +22,24 @@ public class BaseDeDatos extends SQLiteOpenHelper
         Restaurantedb.execSQL("create table Usuario (codigo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,nombre text, apellido text, correo text, usuario text, password text,admin int)");
         Restaurantedb.execSQL("create table Platillo(codigo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,nombre text, descripcion text, precio int)");
         Restaurantedb.execSQL("create table Recomendacion(codigo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, descripcion text)");
-
+        Restaurantedb.execSQL("create table Ordenes(codigo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,nombre text, codigor int, codigop int)");
     }
 
 
     @Override
     public void onUpgrade(SQLiteDatabase Restaurantedb, int oldVersion, int newVersion) {
 
+    }
+    public ArrayList llenarordenes(){
+        ArrayList<String>lista = new ArrayList<>();
+        SQLiteDatabase database = this.getWritableDatabase();
+        String q =  "SELECT * FROM Ordenes";
+        Cursor registros = database.rawQuery(q,null);
+        if(registros.moveToFirst()){
+            do{
+                lista.add(registros.getString(0));
+            }while (registros.moveToNext());
+        }
+        return lista;
     }
 }
