45e9aa00fb4a4857f9221d66834f769db2ce718e
JessyLDA <hinadiaz15@gmail.com>
2021-12-03 22:20:05 -0800

    Ordenes
    Lisview

--- Proyecto 3parcial/app/src/main/java/com/example/proyecto3parcial/ordenes.java
+++ Proyecto 3parcial/app/src/main/java/com/example/proyecto3parcial/ordenes.java
@@ -2,13 +2,30 @@ package com.example.proyecto3parcial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ordenes extends AppCompatActivity {
+
+    ListView lvo;
+    ArrayList<String> listviewOrdenes;
+    ArrayAdapter adaptador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ordenes);
+        lvo=(ListView)findViewById(R.id.listviewOrdenes);
+        BaseDeDatos db = new BaseDeDatos(getApplicationContext(),null,null,1);
+        listviewOrdenes = db.llenarordenes();
+        adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listviewOrdenes);
+        lvo.setAdapter(adaptador);
     }
+
+
 }
